74c322de1262fe0a3110d4724bb07c40b39f5a60
Nurul Arifin <arifinnurul957@gmail.com>
2020-02-28 18:02:29 +0700

    Add Test for User Roles and Permissions

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,11 @@ class Kernel extends HttpKernel
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+        'admin' => [
+            'web',
+            'auth',
+            'role:admin'
+        ]
     ];
 
     /**
@@ -61,6 +66,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
     ];
 
     /**
--- routes/admin.php
+++ routes/admin.php
@@ -10,15 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/admin/dashboard', function () {
     return 'Welcome Admin!';
 })->name('admin.dashboard');
+
+
