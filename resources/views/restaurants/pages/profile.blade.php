c8de725a015ab748c827265332d338f6499e3a30
Nurul Arifin <arifinnurul957@gmail.com>
2020-04-17 23:02:35 +0700

    Created admin setting module

--- resources/views/restaurants/pages/profile.blade.php
+++ resources/views/restaurants/pages/profile.blade.php
@@ -7,7 +7,7 @@
 @stop
 @section('page-title')
 <div class="content-header-left col-md-4 col-12 mb-2">
-    <h3 class="content-header-title">User Profile</h3>
+    <h3 class="content-header-title">User Profile & Setting</h3>
 </div>
 @endsection
 @section('content')
@@ -54,7 +54,7 @@
                         </div>
                         <div class="col-md-6">
                             <input type="text" id="email" value="{{ Auth::user()->email }}" class="form-control" readonly>
-                            @if(Auth::user()->email_verified_at != null)
+                            @if(Auth::user()->email_verified_at == null)
                                 <strong class="font-italic mt-2 text-danger">Your email address is not verified</strong><br>
                                 <button type="button" class="btn btn-primary btn-sm mt-1 mb-1">Verify Now</button>
                                 <button type="button" class="btn btn-success btn-sm mt-1 mb-1">Change Email Address</button>
@@ -64,6 +64,38 @@
                             @endif
                         </div>
                     </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Update Password</h4>
+                <form action="#">
+                    <div class="form-group row">
+                        <div class="col-md-4">
+                            <label for="current_password">{{ __('Current Password') }}</label>
+                        </div>
+                        <div class="col-md-6">
+                            <input name="current_password" id="current_password" type="password" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-4">
+                            <label for="new_password">{{ __('New Password') }}</label>
+                        </div>
+                        <div class="col-md-6">
+                            <input name="new_password" id="new_password" type="password" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-4">
+                            <label for="repeat_new_password">{{ __('Repeat New Password') }}</label>
+                        </div>
+                        <div class="col-md-6">
+                            <input name="repeat_new_password" id="repeat_current_password" type="password" class="form-control">
+                        </div>
+                    </div>
+                    <button class="btn btn-primary btn-min-width my-1" type="submit">Update</button>
+                </form>
             </div>
         </div>
     </div>
